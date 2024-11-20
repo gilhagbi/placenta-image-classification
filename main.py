584455cbeb796155a054f25e740d5218065446c6
gilihagbi <hagbi.gili@gmail.com>
2024-11-20 19:09:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,7 @@ st.title("Medical Images Classification")
 # Get the absolute path to the image
 image_path = os.path.join(os.getcwd(), "robot_image.png")
 
-if os.path.exists(image_path):
-    st.image(image_path, caption="AI Analyzing Medical Images", use_container_width=False, width=300)
-else:
-    st.error("Image file not found.")
-
+st.image("robot_image.png", caption="AI Analyzing Medical Images", width=300)
 # File uploader container
 st.subheader("Upload Medical Images for Classification")
 uploaded_images = st.file_uploader("Upload images", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
