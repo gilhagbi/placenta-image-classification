eaed06b882612e83cb1a6f056bfe59add95729e1
gilihagbi <hagbi.gili@gmail.com>
2024-11-20 20:33:16 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -26,13 +26,15 @@ def crop_with_overlap(image_path, save_path, tile_size=(512, 512), stride=(256, 
     img = Image.open(image_path)
     tiles = []
     save_path = str(save_path)  # Convert save_path to a Path object
-    save_path.mkdir(parents=True, exist_ok=True)
-
+    #save_path.mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     image_path = str(image_path)  # Convert image_path to a Path object
     for i in range(0, img.width - tile_size[0] + 1, stride[0]):
         for j in range(0, img.height - tile_size[1] + 1, stride[1]):
             tile = img.crop((i, j, i + tile_size[0], j + tile_size[1]))
-            tile_path = save_path / f"{image_path.stem}_tile_{i}_{j}.jpg"  # Now image_path is a Path object
+            #tile_path = save_path / f"{image_path.stem}_tile_{i}_{j}.jpg"  # Now image_path is a Path object
+            tile_path = os.path.join(save_path, f"{image_path}_tile_{i}_{j}.jpg")
             tile.save(tile_path)
             tiles.append(tile_path)
 
@@ -42,7 +44,7 @@ def crop_with_overlap(image_path, save_path, tile_size=(512, 512), stride=(256, 
 # Classify tiles and aggregate predictions
 def classify_and_aggregate(learn_inf, image_path, save_path, tile_size=(512, 512), stride=(256, 256)):
     tiles = crop_with_overlap(image_path, save_path, tile_size, stride)
-    predictions = [(tile.name, learn_inf.predict(PILImage.create(tile))) for tile in tiles]  # Unpacking two values
+    predictions = [(os.path.basename(tile), learn_inf.predict(PILImage.create(tile))) for tile in tiles] # Unpacking two values
 
     # Convert probabilities to float32 before stacking and computing mean
     avg_probs = torch.stack([pred[1][1].float() for pred in predictions]).mean(dim=0)
