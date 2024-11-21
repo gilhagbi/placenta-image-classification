062ee6e53bf152abe371e0b298251752a51f11e2
gilihagbi <hagbi.gili@gmail.com>
2024-11-21 14:13:45 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -23,7 +23,7 @@ if uploaded_images:
     for uploaded_image in uploaded_images:
         try:
             # Read image from the uploaded file (in memory)
-            image = Image.open(uploaded_image)
+            image_to_analyze = Image.open(uploaded_image)
             # # Specify save path for cropped tiles
             # save_path = "Image_to_predict"  # Use string paths
             # model_path = os.path.join(os.getcwd(), "placenta_classification_export.pkl")
@@ -45,7 +45,7 @@ if uploaded_images:
                     # st.write(f"### Final Prediction: **{final_prediction}**")
                     # st.subheader(f"Original Image: {uploaded_image.name}")
                     # st.image(image, caption=f"Aggregate Prediction: {final_prediction}", use_container_width=True)
-                    st.image(image, width=600)
+                    st.image(image_to_analyze, width=600)
                 except Exception as e:
                     st.error(f"An error occurred during classification: {e}")
 
