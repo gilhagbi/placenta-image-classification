95815bed2d3811aab8e67f913b49cb2ad51c2497
gilihagbi <hagbi.gili@gmail.com>
2024-11-21 14:15:01 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -45,7 +45,7 @@ if uploaded_images:
                     # st.write(f"### Final Prediction: **{final_prediction}**")
                     # st.subheader(f"Original Image: {uploaded_image.name}")
                     # st.image(image, caption=f"Aggregate Prediction: {final_prediction}", use_container_width=True)
-                    st.image(image_to_analyze, width=600)
+                    st.image(image_to_analyze,use_container_width=True)
                 except Exception as e:
                     st.error(f"An error occurred during classification: {e}")
 
