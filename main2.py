5cb1e9fe0244f3a24d0c825fe32e0da69044b58e
gilihagbi <hagbi.gili@gmail.com>
2024-11-26 22:18:58 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -25,7 +25,7 @@ if uploaded_images:
             image_to_analyze = Image.open(uploaded_image)
             # Specify save path for cropped tiles
             save_path = "Image_to_predict"  # Use string paths
-
+            st.image(image_to_analyze, caption=f"Uploaded Image: {uploaded_image.name}", width=600)
             # Load the model from the pickle file
             model_path = 'model_to_streamlit.pkl'
 
@@ -33,7 +33,7 @@ if uploaded_images:
                 learn_inf = pickle.load(f)
 
             print("Model loaded successfully and is platform-independent.")
-            st.image(image_to_analyze, caption=f"Uploaded Image: {uploaded_image.name}", width=600)
+
             # Load the trained model
             #learn_inf = load_learner(str("placenta_classification_export.pkl"), pickle_module=pickle)
             #learn_inf = load_learner(model_path, pickle_module=pickle)
